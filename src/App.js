3d7644daf5b70794f0abe4ef09a10c14680cf9cb
aminnicevic04 <aminnicevic@gmail.com>
2023-07-14 23:33:40 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
-  return <Expenses />;
+  return (
+    <div>
+      <NewExpense />
+      <Expenses />
+    </div>
+  );
 }
 
 export default App;
